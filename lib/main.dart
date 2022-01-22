e3d651c1344166ff8cb3719e9dcf068d2b3f908d
Nouman Mehmood <noumyy3@gmail.com>
2022-01-22 14:20:41 +0500

    first

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main(){
+
+  runApp(MyApp());
+
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    int days= 30;
+    return MaterialApp(
+      home: Material(
+        child: Center(
+            child: Container(
+                 child: Text('Well come To $days days of Flutter'),
+            ),
+        ),
+
+
+      ),
+      );
+  }
+}
